45d4cca949eb44fdd6a9af92002b57cc574ec7de
Amber Jones <amberlovescats14@gmail.com>
2019-04-21 20:02:40 -0500

    added api with react andincluded like counts for each api craft beer

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {
+  Component
+} from 'react';
 import './App.css';
-import UniqueId from 'react-html-id'
+import Post from './components/Post';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    UniqueId.enableUniqueIds(this);
     this.state = {
       toDos: [],
       id: 0,
@@ -14,31 +14,40 @@ class App extends Component {
       typing: false,
       userHasClicked: 0,
       inputValue: ' ',
-      showLi : false
+      showLi: false
     }
   }
-  handleInput = (e) =>{
+  handleInput = (e) => {
     // console.log(e.target.value)
     this.message(e)
-    this.setState({inputValue: e.target.value})
+    this.setState({
+      inputValue: e.target.value
+    })
   }
-  message = () =>{
-    this.setState({typing: !this.state.typing})
+  message = () => {
+    this.setState({
+      typing: !this.state.typing
+    })
   }
   handleClick = (e) => {
-    this.setState({userHasClicked: this.state.userHasClicked + 1});
+    this.setState({
+      userHasClicked: this.state.userHasClicked + 1
+    });
 
     this.state.toDos.push(this.state.inputValue);
 
-    this.setState({inputValue: ' '});
+    this.setState({
+      inputValue: ' '
+    });
 
-    this.setState({typing: false});
+    this.setState({
+      typing: false
+    });
 
   }
   handleDelete = (e) => {
-    let button = e.target;
-    {
-     button.parentElement.remove(this)
+    let button = e.target; {
+      button.parentElement.remove(this)
     }
   }
 
@@ -46,39 +55,68 @@ class App extends Component {
 
   render() {
     let message;
-    if(this.state.typing){
+    if (this.state.typing) {
       message = "typing..."
     } else {
       message = ''
     }
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src="https://austincodingacademy.com/assets/images/logo-aca-white.png "className="App-logo" alt="logo" />
-          <h1>To Do List</h1>
-          <h4>{message}</h4>
-          <input type='text' onChange={this.handleInput } value={this.state.inputValue}/>
-          <p className='list'>
-            <code>src/App.js</code> 
-          </p>
-          <button className="add" onClick= {this.handleClick} >Add </button>
-          <ol>
-          {this.state.toDos.map((item)=>{
-            return <li className='li'>{item.toUpperCase()} 
-            
-            <button className="x" onClick={this.handleDelete}>X</button></li>
+    return ( <
+      div className = "App" >
+      <
+      header className = "App-header" >
+      <
+      img src = "https://austincodingacademy.com/assets/images/logo-aca-white.png "
+      className = "App-logo"
+      alt = "logo" / >
+      <
+      h1 > To Do List < /h1> <
+      h4 > {
+        message
+      } < /h4> <
+      input type = 'text'
+      onChange = {
+        this.handleInput
+      }
+      value = {
+        this.state.inputValue
+      }
+      /> <
+      p className = 'list' >
+      <
+      code > src / App.js < /code>  <
+      /p> <
+      button className = "add"
+      onClick = {
+        this.handleClick
+      } > Add < /button> <
+      ol > {
+        this.state.toDos.map((item, index) => {
+          return <li className = 'li'
+          key = {
+              index
+            } > {
+              item.toUpperCase()
+            }
 
-          })}
-          </ol>
-        </header>
-      </div>
+            <
+            button className = "x"
+          key = {
+            index
+          }
+          onClick = {
+              this.handleDelete
+            } > X < /button></li >
+
+        })
+      } <
+      /ol> <
+      Post > < /Post>
+
+      <
+      /header> <
+      /div>
     );
   }
 }
-// ReactDOM.render(
-//   <div>
-// <App title='To Do List' />
-// </div>,
-//  document.getElementById('root'))
 
 export default App;
---
+++ to-do-list/src/components/Post.js
@@ -0,0 +1,94 @@
+import React, {
+  Component
+} from 'react'
+
+export default class Post extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      apItem: [],
+      isLoaded: false,
+      count: 0,
+      likes: false,
+      likeCount: 0,
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://api.punkapi.com/v2/beers')
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data)
+        this.setState({
+          apItem: data,
+          isLoaded: true
+        })
+      })
+  }
+  handleLike = (e) => {
+    let classy = e.target.className
+    let counter = document.getElementById(`${classy}`);
+    let value = counter.innerHTML
+    counter.innerHTML = Number(value) + 1
+
+
+  }
+  render() {
+    const style = {
+      textDecoration: 'underline',
+      fontWeight: 800,
+      margins: 0,
+      padding: 0,
+    }
+    const style2 = {
+      color: ' green',
+      fontFamily: 'cursive'
+
+    }
+    return ( <
+      div className = "beer" >
+      <
+      ul > < h1 style = {
+        style
+      } > Beers of the South < /h1> {
+        this.state.apItem.map((beer, i) => {
+          return <li key = {
+              i
+            } >
+            <
+            p className = "beerName" > {
+              beer.name
+            } < /p> <
+            p style = {
+              style2
+            } > "{beer.tagline}" < /p> {
+              beer.description
+            } < br / >
+            <
+            button key = {
+              i
+            }
+          className = {
+            beer.id
+          }
+          onClick = {
+              this.handleLike
+            } > Like < /button>  <
+            div id = {
+              beer.id
+            }
+          key = {
+              beer.id
+            } > {
+              this.state.count
+            } <
+            /div> <
+            /li>
+
+        })
+      } <
+      /ul> <
+      /div>
+    );
+  }
+}
